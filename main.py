88929e1213fc1c06b2a0abb0cf2e9ecf42aee07c
sivanimodali <sivanimodali@gmail.com>
2020-05-19 22:09:02 +0200

    added subtraction feature in master branch

--- main.py
+++ main.py
@@ -25,5 +25,25 @@ def addition():
         return str(float(result))#removed round function and results in float
 
 
+@app.route('/sub')
+def subtraction():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        C = Fraction(value1)
+        D = Fraction(value2)
+        result = C-D
+        return str(float(result))
+
+
+
 if __name__ == "__main__":
     app.run()
