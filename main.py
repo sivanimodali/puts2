78e1a125aef5c31974ee65040166d14cc93a95e2
sivanimodali <sivanimodali@gmail.com>
2020-05-19 22:06:50 +0200

    added subtraction branch

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, request
 from fractions import Fraction
-from decimal import Decimal
+
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
+    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
-@app.route('/add')
-def addition():
+
+@app.route('/sub')
+def subtraction():
     try:
         value1=request.args.get('A',default = 0, type = Fraction)
     except ZeroDivisionError as error:
@@ -21,8 +24,8 @@ def addition():
     else:
         C = Fraction(value1)
         D = Fraction(value2)
-        result = C+D
-        return str(float(result))#removed round function and results in float
+        result = C-D
+        return str(float(result))
 
 
 if __name__ == "__main__":
