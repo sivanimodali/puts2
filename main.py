95b8dd68e0f09a369a22b5709e085cfebfef9cad
sivanimodali <sivanimodali@gmail.com>
2020-05-20 22:22:32 +0200

    cretaed division branch

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, request
 from fractions import Fraction
-from decimal import Decimal
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
+    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
-@app.route('/add')
-def addition():
+
+@app.route('/div')
+def division():
     try:
         value1=request.args.get('A',default = 0, type = Fraction)
     except ZeroDivisionError as error:
@@ -21,46 +23,11 @@ def addition():
     else:
         C = Fraction(value1)
         D = Fraction(value2)
-        result = C+D
-        return str(float(result))#removed round function and results in float
-
-
-@app.route('/sub')
-def subtraction():
-    try:
-        value1=request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value1='None'
-    try:
-        value2=request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value2='None'
-    if value1 == 'None' or value2 == 'None' :
-        return 'None'
-    else:
-        C = Fraction(value1)
-        D = Fraction(value2)
-        result = C-D
-        return str(float(result))
-
-@app.route('/mul')
-def multiplication():
-    try:
-        value1=request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value1='None'
-    try:
-        value2=request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value2='None'
-    if value1 == 'None' or value2 == 'None' :
-        return 'None'
-    else:
-        C = Fraction(value1)
-        D = Fraction(value2)
-        result = C*D
-        return str(float(result))
-
+        try:
+            result = C/D
+            return str(float(result))
+        except ZeroDivisionError as error:
+            return 'None'
 
 
 if __name__ == "__main__":
