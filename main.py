80a8905e2737bd0f405585e495fb54805a238553
sivanimodali <sivanimodali@gmail.com>
2020-05-20 22:19:19 +0200

    added multiplication feature in master branch

--- main.py
+++ main.py
@@ -43,6 +43,24 @@ def subtraction():
         result = C-D
         return str(float(result))
 
+@app.route('/mul')
+def multiplication():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        C = Fraction(value1)
+        D = Fraction(value2)
+        result = C*D
+        return str(float(result))
+
 
 
 if __name__ == "__main__":
